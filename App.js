50d38146efde2db3687f4dc47cd006aea0300a84
Millamarianna <milla.tuomainen@outlook.com>
2023-08-24 12:55:42 +0300

    Tehtävä 1 (Laskin)

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [numberOne, setNumberOne] = useState('');
+  const [numberTwo, setNumberTwo] = useState('');
+  const [result, setResult] = useState('');
+
+  const minusPressed = () => {
+    setResult(parseInt(numberOne) - parseInt(numberTwo))
+  }
+
+  const plusPressed = () => {
+    setResult(parseInt(numberOne) + parseInt(numberTwo))
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Tehtävä valmis</Text>
-      <StatusBar style="auto" />
+
+      
+      <Text style={styles.text}>Result: {result}</Text>
+      <TextInput style={styles.input} keyboardType="numeric" onChangeText={numberOne => setNumberOne(numberOne)} value={numberOne}/>
+      <TextInput style={styles.input} keyboardType="numeric" onChangeText={numberTwo => setNumberTwo(numberTwo)} value={numberTwo}/>
+      
+      <View style={styles.button}>
+      <Button onPress={minusPressed} title=" - " />
+      <Button onPress={plusPressed} title=" + " />
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 20,
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 100
   },
+  text : {
+    width:200, 
+    textAlign: 'center',
+    fontWeight: 'bold',
+    margin: 5,
+  },
+  input : {
+    width:200, 
+    fontWeight: 'bold',
+    borderColor: 'grey', 
+    margin: 5,
+    borderWidth: 1
+  },
+  button: {
+    alignItems: 'center',
+    height: 40,
+    width: 200,
+    marginTop: 20,
+    justifyContent: 'center',
+    flexDirection: 'row'
+  }
 });
