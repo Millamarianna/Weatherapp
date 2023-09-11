ee2e2856e0c165733cf40dacaf8045b55c5adaa9
Millamarianna <milla.tuomainen@outlook.com>
2023-09-11 10:05:39 +0300

    Tehtävä 6 (Reseptien haku)

--- App.js
+++ App.js
@@ -1,55 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet} from 'react-native';
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Calculator from './Calculator';
-import History from './History';
-
-const Stack = createNativeStackNavigator();
+import React, { useState } from 'react';
+import { Alert, StyleSheet, Text, View, Button, TextInput, FlatList, StatusBar, Image } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [repositories, setRepositories] = useState([]);
+ 
+  const getRepositories = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+    .then(response => response.json())
+    .then(responseJson => setRepositories(responseJson.meals))
+    .catch(error => { 
+        Alert.alert('Error:', error.message); 
+    });    
+  }
   
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen
-          name="Calculator"
-          component={Calculator}
-          options={{title: 'Calculator'}}
-        />
-        <Stack.Screen name="History" component={History} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <StatusBar hidden={true} />
+      <FlatList 
+        style={{marginLeft : "5%"}}
+        keyExtractor={(item, index) => index.toString()} 
+        renderItem={({item}) => 
+          <View>
+            <Text style={{fontSize: 18, fontWeight: "bold"}}>{item.strMeal}</Text>
+            <Image style={styles.tinyLogo} source={{uri: item.strMealThumb,}}/>
+          </View>}
+        data={repositories} 
+        ItemSeparatorComponent={listSeparator} /> 
+      <TextInput 
+        style={{fontSize: 18, width: 200}} 
+        placeholder='keyword' 
+        value={keyword}
+        onChangeText={text => setKeyword(text)} 
+       />
+      <Button title="Find" onPress={getRepositories} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 20,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    padding: 100
-  },
-  text : {
-    width:200, 
-    textAlign: 'center',
-    fontWeight: 'bold',
-    margin: 5,
-  },
-  input : {
-    width:200, 
-    fontWeight: 'bold',
-    borderColor: 'grey', 
-    margin: 5,
-    borderWidth: 1
-  },
-  button: {
-    alignItems: 'center',
-    margin: 20,
-    gap: 10,
-    justifyContent: 'center',
-    flexDirection: 'row'
-  }
+ container: {
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
+ },
+ tinyLogo: {
+  width: 50,
+  height: 50,
+},
 });
