1ca4cf2fe8f4ae98bb56c1ed7091b72f149526a3
Millamarianna <milla.tuomainen@outlook.com>
2023-09-21 13:39:07 +0300

    Tehtävä 8 (Etsi osoite)

--- App.js
+++ App.js
@@ -1,97 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { ActivityIndicator, Alert, StyleSheet, Text, View, Button, TextInput, StatusBar, Image } from 'react-native';
-import {Picker} from '@react-native-picker/picker';
-import Fetch from './Fetch'
+import React, { useState } from 'react';
+import { Alert, StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 
 export default function App() {
-  const [amount, setAmount] = useState('');
-  const [repositories, setRepositories] = useState([]);
-  const [selectedCurrency, setSelectedCurrency] = useState();
-  const [converted, setConverted] = useState(0);
-  const [currencyData, setCurrencyData] = useState([]);
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+  const [title, setTitle] = useState('');
   const [isLoading, setLoading] = useState(true);
-  const [currencies, setCurrencies] = useState([]);
 
-
-
-useEffect(() => {
-setCurrencies(Object.keys(currencyData));
-setLoading(false)
-  }, []);
-
-  useEffect(() => {
-    console.log(currencyData);
-    setCurrencies(Object.keys(currencyData));
-    setLoading(false)
-      }, [currencyData]);
- 
-  const getRepositories = () => {
-    fetch(`https://api.apilayer.com/exchangerates_data/latest?symbols=EUR&base=${selectedCurrency}`, 
-    {
-      method: 'GET',
-      redirect: 'follow',
-      headers: {"apikey": "36gVmYqt1foS8uTz3RgDzc4mw8uRdD1f"}
-    })
-    .then(response => response.json())
-    .then(responseJson => setRepositories(responseJson.rates.EUR))
-    .catch(error => { 
-        Alert.alert('Error:', error.message); 
-    });  
-    setConverted(repositories * amount);
+  const getCoordinates = async () => {
+    const url = `https://www.mapquestapi.com/geocoding/v1/address?key=XyIkq6OIN8V6qYs6m9qm83fLLkSAU5aS&location=${title}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    };    
   }
 
+  const getAddress = async () => {
+    try {
+      const data = await getCoordinates();
+      setLatitude(data.results[0].locations[0].latLng.lat);
+      setLongitude(data.results[0].locations[0].latLng.lng);
+      setLoading(false)
+
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    };
+  }
 
   return (<View style={styles.container}>
-        <Fetch setCurrencyData={setCurrencyData} />
-        {isLoading ? (
-          <ActivityIndicator />
-        ) : (
-          <View style={styles.container}>
-            <StatusBar hidden={true} />
-          
-          <Text style={{fontSize: 17, fontWeight: "bold"}}>{converted}</Text>
-          <Image style={styles.tinyLogo} source={require('./euro.png')}/> 
-      <TextInput 
-        style={{fontSize: 18, 
-          height:50, 
-          width: 250, 
-          borderTopWidth : 1,
+    
+    {isLoading ? (<Text>Search for an address</Text>) : (
+    <MapView
+    style={{...StyleSheet.absoluteFillObject}}
+    region={{
+      latitude: parseFloat(latitude),
+      longitude: parseFloat(longitude),
+      latitudeDelta: 0.0322,
+      longitudeDelta: 0.0221,
+    }}> 
+    <Marker 
+    coordinate={{
+      latitude: parseFloat(latitude),
+      longitude: parseFloat(longitude)
+    }} />
+    </MapView>
+    )}
+   
+        <TextInput
+        style={{
+          backgroundColor: '#fff',
+          fontSize: 18,
+          height: 50,
+          width: 250,
+          borderTopWidth: 1,
           borderLeftWidth: 1,
           borderRightWidth: 2,
-          borderBottomWidth : 2}} 
-        keyboardType="numeric"
-        placeholder='Type amount to be converted' 
-        value={amount}
-        onChangeText={numbers => setAmount(numbers)} 
-       />
-      
-      <Picker 
-      style={{height:40, width:150}}
-        selectedValue={selectedCurrency} 
-        onValueChange={(itemValue, itemIndex) => setSelectedCurrency(itemValue)}>
-          {
-            currencies.map(currency => <Picker.Item key={currency} label={currency} value={currency}/>)
-          }
-      </Picker>
-      <Button title="Convert" onPress={getRepositories} />
-      </View>)}
-      
-    </View>   
+          borderBottomWidth: 2,
+        }}
+        placeholder='Type an address'
+        value={title}
+        onChangeText={title => setTitle(title)}
+      />
+      <Button title="Search" onPress={getAddress} />
+     
+  </View>
   );
 }
 
-   
+
 
 const styles = StyleSheet.create({
- container: {
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
- },
- tinyLogo: {
-  width: 100,
-  height: 100,
-},
+  container: {
+    flex: 2,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
 });
