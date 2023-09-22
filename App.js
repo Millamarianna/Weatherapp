19f9d0351fc9fc39e25e35b8f881f3eb609e105c
Millamarianna <milla.tuomainen@outlook.com>
2023-09-22 10:40:57 +0300

    Tehtävä 10 (Etsi osoite & sijainti)

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
-import { Alert, StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, Alert, StyleSheet, View, Button, TextInput } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 
 export default function App() {
-  const [latitude, setLatitude] = useState();
-  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
   const [title, setTitle] = useState('');
-  const [isLoading, setLoading] = useState(true);
+  const [ready, setReady] = useState(true)
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('No permission to get location')
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+      setReady(false);
+    })();
+  }, []);
+
 
   const getCoordinates = async () => {
     const url = `https://www.mapquestapi.com/geocoding/v1/address?key=XyIkq6OIN8V6qYs6m9qm83fLLkSAU5aS&location=${title}`;
@@ -17,7 +33,7 @@ export default function App() {
       return data
     } catch (error) {
       Alert.alert('Error', error.message)
-    };    
+    };
   }
 
   const getAddress = async () => {
@@ -25,59 +41,67 @@ export default function App() {
       const data = await getCoordinates();
       setLatitude(data.results[0].locations[0].latLng.lat);
       setLongitude(data.results[0].locations[0].latLng.lng);
-      setLoading(false)
-
     } catch (error) {
       Alert.alert('Error', error.message)
     };
   }
 
   return (<View style={styles.container}>
-    
-    {isLoading ? (<Text>Search for an address</Text>) : (
-    <MapView
-    style={{...StyleSheet.absoluteFillObject}}
-    region={{
-      latitude: parseFloat(latitude),
-      longitude: parseFloat(longitude),
-      latitudeDelta: 0.0322,
-      longitudeDelta: 0.0221,
-    }}> 
-    <Marker 
-    coordinate={{
-      latitude: parseFloat(latitude),
-      longitude: parseFloat(longitude)
-    }} />
-    </MapView>
-    )}
-   
-        <TextInput
-        style={{
-          backgroundColor: '#fff',
-          fontSize: 18,
-          height: 50,
-          width: 250,
-          borderTopWidth: 1,
-          borderLeftWidth: 1,
-          borderRightWidth: 2,
-          borderBottomWidth: 2,
-        }}
-        placeholder='Type an address'
-        value={title}
-        onChangeText={title => setTitle(title)}
-      />
-      <Button title="Search" onPress={getAddress} />
-     
-  </View>
-  );
+    {ready ? (<ActivityIndicator size="large" />) :
+      (
+          <MapView
+            style={{ ...StyleSheet.absoluteFillObject }}
+            initialRegion={{
+              latitude: parseFloat(latitude),
+              longitude: parseFloat(longitude),
+              latitudeDelta: 0.0322,
+              longitudeDelta: 0.0221,
+            }}
+            region={{
+              latitude: parseFloat(latitude),
+              longitude: parseFloat(longitude),
+              latitudeDelta: 0.0322,
+              longitudeDelta: 0.0221,
+            }}
+          >
+              <Marker
+                coordinate={{
+                  latitude: parseFloat(latitude),
+                  longitude: parseFloat(longitude)
+                }} />
+          </MapView>)}
+
+          <TextInput
+            style={{
+              backgroundColor: '#fff',
+              fontSize: 18,
+              height: 50,
+              width: 250,
+              borderTopWidth: 1,
+              borderLeftWidth: 1,
+              borderRightWidth: 2,
+              borderBottomWidth: 2,
+            }}
+            placeholder='Type an address'
+            value={title}
+            onChangeText={title => setTitle(title)}
+          />
+          <Button title="Search" onPress={getAddress} />
+      
+  </View>);
 }
 
 
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
+    flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  inner: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-end',
   },
